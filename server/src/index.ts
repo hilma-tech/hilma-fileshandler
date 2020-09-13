66dccd35de42fb4f302a5a61273a123ed3818dc7
michaelseg11 <michaelseg11@gmail.com>
2020-09-13 11:21:41 +0300

    start creating an npm module

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+
+//common
+export * from './fileshandler/common/decorators/filesHandler.decorator';
+export * from './fileshandler/common/interfaces/fIlesHandlerOptions.interface';
+export * from './fileshandler/common/types/permissionsFilter.type';
+
+//fileshandler module
+export * from './fileshandler/file/fileshandler.module';
+
+//services
+export * from './fileshandler/file/services/upload/image.service';
+export * from './fileshandler/file/services/upload/audio.service';
+export * from './fileshandler/file/services/upload/file.service';
+export * from './fileshandler/file/services/upload/video.service';
+
+//filePermission module
+export * from './fileshandler/filePermission/filePermission.module';
+
+//entity
+export * from './fileshandler/filePermission/filePermission.entity';
+
+//enums
+export * from './fileshandler/filePermission/enums/permission.enum';
+export * from './fileshandler/filePermission/enums/permissionType.enum';
+
+//service
+export * from './fileshandler/filePermission/filePermission.service';
