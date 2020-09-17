9c9d10c1c868fe3b877588d4a9f6eb4869cfec2d
michaelseg11 <michaelseg11@gmail.com>
2020-09-17 12:18:01 +0300

    global module

--- server/src/fileshandler/file/fileshandler.module.ts
+++ server/src/fileshandler/file/fileshandler.module.ts
@@ -65,6 +65,7 @@ export class FilesHandlerModule {
         FilesHandlerModule.createFolders(options);
 
         return {
+            global: true, // think more about it
             module: FilesHandlerModule,
             providers: [
                 {
