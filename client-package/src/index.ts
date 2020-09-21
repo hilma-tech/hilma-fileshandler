976b4bc6a2cd7c850e54e122877bfddf34c2671f
michaelseg11 <michaelseg11@gmail.com>
2020-09-21 13:01:37 +0300

    add MultipleFilesInput to index.ts

--- client-package/src/index.ts
+++ client-package/src/index.ts
@@ -3,3 +3,4 @@ export { default as FilesUploader } from './FilesUploader';
 export { default as UploadedFile } from './UploadedFile.interface';
 export { default as useFiles } from './useFiles';
 export { default as withFiles } from './withFiles';
+export { default as MultipleFilesInput } from './MultipeFilesInput';
