bbdf76ee1d27c30200c5996425b116852cd8105c
michaelseg11 <michaelseg11@gmail.com>
2020-09-30 12:16:05 +0300

    no saveClientFilesInterceptor

--- server/src/fileshandler/common/decorators/filesHandler.decorator.ts
+++ server/src/fileshandler/common/decorators/filesHandler.decorator.ts
@@ -1,7 +1,7 @@
 import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { GetBodyInterceptor } from '../interceptors/getBody.interceptor';
-import { SaveClientFilesInterceptor } from '../interceptors/saveClientFiles.interceptor'
+// import { SaveClientFilesInterceptor } from '../interceptors/saveClientFiles.interceptor'
 
 export function UseFilesHandler() {
     return applyDecorators(
