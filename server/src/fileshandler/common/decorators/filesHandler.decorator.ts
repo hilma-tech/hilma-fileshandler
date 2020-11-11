af1dc25c0e39e28273f805e3698115c7191fdabe
michaelseg11 <michaelseg11@gmail.com>
2020-11-11 13:32:46 +0200

    delete old interceptor

--- server/src/fileshandler/common/decorators/filesHandler.decorator.ts
+++ server/src/fileshandler/common/decorators/filesHandler.decorator.ts
@@ -1,14 +1,12 @@
 import { applyDecorators, UseInterceptors } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { GetBodyInterceptor } from '../interceptors/getBody.interceptor';
-// import { SaveClientFilesInterceptor } from '../interceptors/saveClientFiles.interceptor'
 
 export function UseFilesHandler() {
     return applyDecorators(
         UseInterceptors(
             FilesInterceptor("FilesHandler"),
             GetBodyInterceptor,
-            // SaveClientFilesInterceptor
         )
     );
 }
