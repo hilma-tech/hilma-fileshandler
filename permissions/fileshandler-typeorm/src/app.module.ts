75ce53a79cf06d7b45814e89f108e83ba420c64f
michaelseg11 <michaelseg11@gmail.com>
2020-11-15 13:51:50 +0200

    add mysql as dev-dependency for typeorm

--- permissions/fileshandler-typeorm/src/app.module.ts
+++ permissions/fileshandler-typeorm/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesHandlerTypeormModule } from './fileshandler-typeorm/fileshandler-typeorm.module';
+import { FilePermission } from './fileshandler-typeorm/filePermission/filePermission.entity';
 
 @Module({
   imports: [
@@ -13,7 +15,19 @@ import { FilesHandlerTypeormModule } from './fileshandler-typeorm/fileshandler-t
         m: 400,
         l: 600
       },
-      
+
+    }),
+    TypeOrmModule.forRoot({
+      database: "nest_fileshandler",
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "z10mz10m",
+      synchronize: true,
+      entities: [
+        FilePermission
+      ]
     })
   ],
   controllers: [AppController],
