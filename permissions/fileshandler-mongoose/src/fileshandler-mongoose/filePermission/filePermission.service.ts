59a67faad778fadbe73a60201491e115749adf89
michaelseg11 <michaelseg11@gmail.com>
2020-11-16 10:03:46 +0200

    create add and remove permissions

--- permissions/fileshandler-mongoose/src/fileshandler-mongoose/filePermission/filePermission.service.ts
+++ permissions/fileshandler-mongoose/src/fileshandler-mongoose/filePermission/filePermission.service.ts
@@ -1,13 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FilePermission, FilePermissionDocument } from './filePermission.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class FilePermissionService {
-    constructor(@InjectModel(FilePermission.name) private readonly filePermissionModel: Model<FilePermissionDocument>) {
-        const fp = new this.filePermissionModel({ path: "image/;cfldscdasd.png" });
-        fp.save()
+    constructor(
+        @InjectModel(FilePermission.name) private readonly filePermissionModel: Model<FilePermissionDocument>
+    ) { }
+
+    public async saveUsersPermission(path: string, userIds: Types.ObjectId[]): Promise<void> {
+        const permission = new this.filePermissionModel();
+        permission.path = path;
+        permission.allowedUsers = userIds;
+        await permission.save();
+    }
+
+    public async saveRolesPermission(path: string, roles: string[]): Promise<void> {
+        const permission = new this.filePermissionModel();
+        permission.path = path;
+        permission.allowedRoles = roles;
+        await permission.save();
+    }
+
+    public async addPermissions<T extends keyof Omit<FilePermission, "path" | "_id">>(path: string, permissionName: T, array: FilePermission[T]): Promise<void> {
+        await this.filePermissionModel.updateOne({
+            path
+        }, {
+            $addToSet: {
+                [permissionName]: array
+            }
+        }).exec();
+    }
+
+    public async removePermissions<T extends keyof Omit<FilePermission, "path" | "_id">>(path: string, permissionName: T, array: FilePermission[T]): Promise<void> {
+        await this.filePermissionModel.updateOne({
+            path
+        }, {
+            $pullAll: {
+                [permissionName]: array
+            }
+        }).exec();
     }
 
 }
