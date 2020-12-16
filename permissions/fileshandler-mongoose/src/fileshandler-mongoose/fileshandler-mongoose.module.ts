4ed6125b6eb0b39f6299ea2b0483af011eaedc1b
michaelseg11 <michaelseg11@gmail.com>
2020-12-16 10:43:24 +0200

    add FilePermissionModule to exports so it can be used without importing it - it is now global as the other services

--- permissions/fileshandler-mongoose/src/fileshandler-mongoose/fileshandler-mongoose.module.ts
+++ permissions/fileshandler-mongoose/src/fileshandler-mongoose/fileshandler-mongoose.module.ts
@@ -24,6 +24,7 @@ import { FilePermission, FilePermissionSchema } from './filePermission/filePermi
         AudioMongooseService,
         FileMongooseService,
         VideoMongooseService,
+        FilePermissionModule
     ]
 })
 export class FilesHandlerMongooseModule {
