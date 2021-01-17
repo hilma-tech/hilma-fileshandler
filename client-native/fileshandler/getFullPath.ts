157a8baaaf5e7717739d4bf436103d70ffa2bbc9
michaelseg11 <michaelseg11@gmail.com>
2021-01-17 14:27:30 +0200

    client-native - uploaded image + auth-native

---
+++ client-native/fileshandler/getFullPath.ts
@@ -0,0 +1,8 @@
+const variables = require("../../../../../variables")["default"];
+const apiUrl: string = variables.apiUrl;
+
+const getFullPath = (path: string): string => {
+    return apiUrl + path;
+}
+
+export default getFullPath;
